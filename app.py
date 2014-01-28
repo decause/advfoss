4f0e02fc067d6a8a4856948f065ee2dd2eb9a1f9
RemyD <remyd@civx.us>
2014-01-28 16:55:13 -0500

    Added main app

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+
+from advfoss.site import app
+
+if __name__ == "__main__":
+    if 'OPENSHIFT_PYTHON_IP' in os.environ:
+        host = os.environ['OPENSHIFT_PYTHON_IP']
+        port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+        app.run(host=host, port=port)
+    else:
+        app.debug = True
+        app.run()
